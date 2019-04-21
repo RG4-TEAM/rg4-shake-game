1de71c7680aa1a084f49eda992f8abb1b5ef7052
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-04-21 23:38:55 +0700

    fix index redirect to register

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-<?php include("./header.php"); ?>
-  <form>
-    <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <div class="form-group form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="button" class="btn btn-primary" onclick="saveOnClick()">Save</button>
-    <button type="button" class="btn btn-warning" onclick="editData()">Edit</button>
-    <button type="button" class="btn btn-danger">Delete</button>
-  </form>
-  <ul id="userList"></ul>
-  <script src="./app.js"></script>
-<?php include("./footer.php"); ?>
+<?php 
+  header("location: register.php");
+?>
